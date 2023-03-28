f0b30d112ee11035ee5ae2514d7554592cf8ee54
Krista M <morales-k@outlook.com>
2023-03-28 14:14:35 -0400

    Added texture and controls
    Created assets folder with enviornment map texture. Added orbital controls.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,8 @@
 import { Scene, PerspectiveCamera, WebGLRenderer, Color, 
 ACESFilmicToneMapping, sRGBEncoding, Mesh, 
-SphereGeometry, MeshBasicMaterial } from "three";
+SphereGeometry, MeshStandardMaterial, PMREMGenerator, FloatType } from "three";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
@@ -16,14 +18,31 @@ renderer.physicallyCorrectLights = true;
 
 document.body.appendChild(renderer.domElement);
 
-let sphereMesh = new Mesh(
-  new SphereGeometry(5, 10, 10),
-  new MeshBasicMaterial({ color: 0xff0000})
-);
-scene.add(sphereMesh);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+controls.dampingFactor = 0.05;
+controls.enableDamping = true;
+
+let envmap;
 
 const loop = async () => {
+  // Process enviornment map
+  let pmrem = new PMREMGenerator(renderer);
+  let envmapTexture = await new RGBELoader().setDataType(FloatType).loadAsync("assets/envmap.hdr");
+  envmap = pmrem.fromEquirectangular(envmapTexture).texture;
+
+  let sphereMesh = new Mesh(
+    new SphereGeometry(5, 10, 10),
+    new MeshStandardMaterial({
+      envMap: envmap,
+      roughness: 0,
+      metalness: 1,
+    })
+  );
+  scene.add(sphereMesh);
+
   renderer.setAnimationLoop(() => {
+    controls.update();
     renderer.render(scene, camera);
   });
 }
